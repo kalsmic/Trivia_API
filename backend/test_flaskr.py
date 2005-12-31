0b02a0a4d6068bc0f807e585af6329d74032c831
Arthur Kalule <kalulearthur@gmail.com>
2006-01-01 01:32:12 +0300

    test setup

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,10 +12,16 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
+        self.app = create_app({'TESTING': True})
+        self.client = self.app.test_client()
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_username = "postgres"
+        self.database_password = "root"
+
+        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = f"postgres://{self.database_username}:{self.database_password}@localhost:5432/trivia_test"
+
+
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +39,16 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_questions(self):
+       response = self.client.get('/questions')
+       data = json.loads(response.data.decode())
+       self.assertEqual(response.status_code, 200)
+       self.assertTrue(isinstance(data['questions'], list))
+       self.assertTrue(isinstance(data['categories'], list))
+
+
+    #    assert qn.status == 200
 
 
 # Make the tests conveniently executable
